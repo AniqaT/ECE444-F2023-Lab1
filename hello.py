acb387466afac0ef0b061ad67e8977e1351caabd
AniqaT <aniqatahseen1@gmail.com>
2023-09-22 15:54:55 -0400

    Activity 3 is complete and local date and time now is displayed on the user and home pages, also added the nav bar code to the base.html file

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -14,12 +15,19 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
-'''
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+'''
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                          current_time=datetime.utcnow())
+
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name, 
+                           current_time=datetime.utcnow())
