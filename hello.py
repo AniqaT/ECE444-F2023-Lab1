6c55e2a0f3bf6889a0cd9f5e922090aec30df6ec
AniqaT <aniqatahseen1@gmail.com>
2023-09-22 18:25:07 -0400

    Added code as shown in example 4-7

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -29,21 +29,25 @@ def index():
 def index():
     return render_template('index.html',
                           current_time=datetime.utcnow())
-'''
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    name = None
-    form = NameForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name, 
                            current_time=datetime.utcnow())
+'''
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',
+        form = form, name = session.get('name'))
+
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
